77734d1074947d7b2bac1a1fcb530621459f6403
Oswaldo <oswaldo.dev.oliveira@gmail.com>
2022-08-23 14:49:11 -0300

    reparo no jsonFormat

--- services/ONG/src/main/java/br/com/compasso/ONG/dto/response/ResponseOngDto.java
+++ services/ONG/src/main/java/br/com/compasso/ONG/dto/response/ResponseOngDto.java
@@ -1,6 +1,6 @@
 package br.com.compasso.ONG.dto.response;
 
-import br.com.compasso.ONG.entity.Address;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -10,6 +10,7 @@ public class ResponseOngDto {
 
     private String Cnpj;
     private String name;
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate foundationDate;
     private ResponseAddressDto address;
     private Integer amountCat;
