e9fc12445899b89ebd66477135d684e8085a1ca5
Oswaldo <oswaldo.dev.oliveira@gmail.com>
2022-08-29 16:58:58 -0300

    Parte de email concluida

---
+++ services/email/src/test/java/br/com/compass/email/EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
