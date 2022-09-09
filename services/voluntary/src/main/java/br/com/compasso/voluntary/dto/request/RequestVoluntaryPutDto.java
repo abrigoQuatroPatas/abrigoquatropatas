020ceb06debe286a9c29f4218d264e9e1c03dc86
Nicolas Bernardes <nicolasbernardes84@gmail.com>
2022-09-09 18:19:25 -0300

    PutDto Adicionado

---
+++ services/voluntary/src/main/java/br/com/compasso/voluntary/dto/request/RequestVoluntaryPutDto.java
@@ -0,0 +1,35 @@
+package br.com.compasso.voluntary.dto.request;
+
+
+import br.com.compasso.voluntary.enums.StatusEnum;
+import br.com.compasso.voluntary.enums.TypeEnum;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class RequestVoluntaryPutDto {
+
+    @NotBlank(message = "Name field cannot be blank")
+    private String name;
+    @NotNull(message = "Type field cannot be blank")
+    private TypeEnum type;
+    @JsonFormat(pattern="dd/MM/yyyy")
+    @NotNull(message = "BirthDate field cannot be null")
+    private LocalDate birthDate;
+    @Valid
+    private RequestAddressDto address;
+    @NotNull(message = "Status field cannot be blank")
+    private StatusEnum status;
+
+}
